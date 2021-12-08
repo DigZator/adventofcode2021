380ec8d9b5dfcbb25db22c9d62dba5cce97c7ea4
DigZator <omsbhise@gmail.com>
2021-12-08 22:00:25 +0530

    Completed s13.py and s14.py

---
+++ day7/s13.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+	with open('input.txt') as f:
+		crab_loc = []
+		crab_loc = [int(x) for x in f.readline().split(",")]
+		#print(crab_loc)
+		most = max(crab_loc)
+		least = min(crab_loc)
+		#print(most,least)
+		dis = 99999999999
+		for i in range(least, most):
+			dist = sum(abs(i-d) for d in crab_loc)
+			if dis > dist:
+				dis = dist
+
+		print(dis)

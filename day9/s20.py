fa8bafb2a3b893e5eddb858f512564f63e042471
DigZator <87714053+DigZator@users.noreply.github.com>
2021-12-12 15:31:00 +0530

    Changes in s20.py

--- day9/s20.py
+++ day9/s20.py
@@ -1,1 +1,36 @@
+class Grid:
+    def __init__(self, board):
+        self.grid = board
+        self.R = len(self.grid)
+        self.C = len(self.grid[0])
 
+    def is_valid_coord(self,r,c):
+    	return r in range(self.R) and c in range(self.C)
+
+    def neighbour_coord(self, r,c):
+    	n = []
+    	if is_valid_coord(r-1,c):
+    		n.append([r-1,c])
+    	if is_valid_coord(r+1,c):
+    		n.append([r+1,c])
+    	if is_valid_coord(r,c+1):
+    		n.append([r,c]+1)
+    	if is_valid_coord(r,c-1):
+    		n.append([r,c-1])
+    	return n
+
+    def 
+    	
+
+
+if __name__ == "__main__":
+	with open('input.txt') as f:
+		board = []
+		for line in f.readlines():
+			row = []
+			line = line.strip()
+			for x in line:
+				row.append(int(x))
+			board.append(row)
+		g = Grid(board)
+		print(g.grid)

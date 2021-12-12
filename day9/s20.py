1bba51f015c7618085afbd92350f41104899176a
DigZator <omsbhise@gmail.com>
2021-12-13 03:21:01 +0530

    Completed s20.py

--- day9/s20.py
+++ day9/s20.py
@@ -1,42 +1,47 @@
-class Grid:
-    def __init__(self, board):
-        self.grid = board
-        self.R = len(self.grid)
-        self.C = len(self.grid[0])
+with open('input.txt', 'r') as f:
+	lines = f.read().splitlines()
 
-    def is_valid_coord(self,r,c):
-    	return r in range(self.R) and c in range(self.C)
+height_map = {}
 
-    def neighbour_coord(self, r,c):
-    	n = []
-    	if is_valid_coord(r-1,c):
-    		n.append([r-1,c])
-    	if is_valid_coord(r+1,c):
-    		n.append([r+1,c])
-    	if is_valid_coord(r,c+1):
-    		n.append([r,c]+1)
-    	if is_valid_coord(r,c-1):
-    		n.append([r,c-1])
-    	return n
+for y, row in enumerate(lines):
+	for x, height in enumerate(row):
+		height_map[(x,y)] = int(height)
 
-    def 
-    	
+#print(height_map)
+#print(height_map.items())
+low = []
 
+for coord, height in height_map.items():
+	x,y = coord
+	near = ((x+1,y),(x-1,y),(x,y+1),(x,y-1))
+	small = True
+	for adj in near:
+		if height_map.get(adj, 10) <= height:
+			small = False
+			break
+	if small:
+		low.append(coord)
 
-if __name__ == "__main__":
-	with open('input.txt') as f:
-		#board = []
-		#for line in f.readlines():
-		#	row = []
-		#	line = line.strip()
-		#	for x in line:
-		#		row.append(int(x))
-		#	board.append(row)
-		#g = Grid(board)
-		#print(g.grid)
-		rows = [line.strip() for line in f.readline()]
-	height_map = {}
-	for y, row in enumerate(rows):
-		for x, height in enumerate(row):
-			height_map[(x,y)] = int(height)
-	print(height_map)
+#print(low)
+
+basin_sizes = []
+
+for point in low:
+	part = set([point])
+	check = [point]
+	while check:
+		x, y = check.pop()
+		near = ((x+1,y),(x-1,y),(x,y+1),(x,y-1))
+		for adj in near:
+			adj_height = height_map.get(adj, 0)
+			if adj_height > height_map[(x,y)] and adj_height != 9:
+				part.add(adj)
+				check.append(adj)
+
+	basin_sizes.append(len(part))
+
+basin_sizes.sort(reverse = True)
+#print(basin_sizes)
+#print(basin_sizes[0],basin_sizes[1],basin_sizes[2])
+print(basin_sizes[0]*basin_sizes[1]*basin_sizes[2])
+
